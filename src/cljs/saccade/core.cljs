e4da87930546add49d8df97ae3541d9d38f8f04e
Marcus Lewis <mrcslws@gmail.com>
2014-10-30 01:33:57 -0700

    Pull the px size into the global state

--- src/cljs/saccade/core.cljs
+++ src/cljs/saccade/core.cljs
@@ -24,23 +24,25 @@
 
 (def app-state
   (atom
-   {:world [[0 0 0 0 0 0 0 0 0]
-            [0 0 0 0 0 0 0 0 0]
-            [0 0 0 0 0 0 0 0 0]
-            [0 0 0 0 0 0 0 0 0]
-            [0 0 0 0 1 0 0 0 0]
-            [0 0 0 0 0 1 0 0 0]
-            [0 0 0 0 0 0 0 0 0]
-            [0 0 0 0 0 0 0 0 0]
-            [0 0 0 0 0 0 0 0 0]]
+   {:world {:bitmap [[0 0 0 0 0 0 0 0 0]
+                     [0 0 0 0 0 0 0 0 0]
+                     [0 0 0 0 0 0 0 0 0]
+                     [0 0 0 0 0 0 0 0 0]
+                     [0 0 0 0 1 0 0 0 0]
+                     [0 0 0 0 0 1 0 0 0]
+                     [0 0 0 0 0 0 0 0 0]
+                     [0 0 0 0 0 0 0 0 0]
+                     [0 0 0 0 0 0 0 0 0]]
+            :width-px 500
+            :height-px 500}
     :observer {:xi 3 :yi 3 :width 3 :height 3
                :server-token nil}}))
 
-(defn world-width [world]
-  (count world))
+(defn bitmap-width [bitmap]
+  (count bitmap))
 
-(defn world-height [world]
-  (count (first world)))
+(defn bitmap-height [bitmap]
+  (count (first bitmap)))
 
 ;; ============================================================================
 ;; Component: world-view
@@ -53,12 +55,12 @@
 
 (def canvas-ref "world-canvas")
 
-(defn paint-world [world owner]
+(defn paint-world [{:keys [bitmap width-px height-px]} owner]
   (let [ctx (.getContext (om/get-node owner canvas-ref) "2d")
-        wi (world-width world)
-        hi (world-height world)
-        wpcell (/ (om/get-state owner :width) wi)
-        hpcell (/ (om/get-state owner :height) hi)]
+        wi (bitmap-width bitmap)
+        hi (bitmap-height bitmap)
+        wpcell (/ width-px wi)
+        hpcell (/ height-px hi)]
     (clear-canvas ctx)
     (set! (.-fillStyle ctx) "black")
     (set! (.-strokeStyle ctx) "#2E7DD1")
@@ -68,12 +70,12 @@
             :let [xp (* xi wpcell)
                   yp (* yi hpcell)]]
       ;; Paint the dot, if applicable.
-      (when (= 1 (-> world (nth xi) (nth yi)))
+      (when (= 1 (-> bitmap (nth xi) (nth yi)))
         (.fillRect ctx xp yp wpcell hpcell))
       ;; Paint the grid.
       (.strokeRect ctx xp yp wpcell hpcell))))
 
-(defn world-view [world owner]
+(defn world-view [{:keys [width-px height-px] :as world} owner]
   (reify
     om/IDidMount
     (did-mount [_]
@@ -84,8 +86,8 @@
       (paint-world world owner))
 
     om/IRenderState
-    (render-state [_ {:keys [width height style]}]
-      (dom/canvas #js {:ref canvas-ref :width width :height height
+    (render-state [_ {:keys [style]}]
+      (dom/canvas #js {:ref canvas-ref :width width-px :height height-px
                        :style (clj->js style)}))))
 
 
@@ -104,11 +106,11 @@
         remainder (mod magnitude interval)]
     (* direction (- magnitude (/ remainder 2)))))
 
-(defn cell-width [world owner]
-  (/ (om/get-state owner :width) (world-width world)))
+(defn cell-width [{:keys [bitmap width-px]} owner]
+  (/ width-px (bitmap-width bitmap)))
 
-(defn cell-height [world owner]
-  (/ (om/get-state owner :height) (world-height world)))
+(defn cell-height [{:keys [bitmap height-px]} owner]
+  (/ height-px (bitmap-height bitmap)))
 
 (defn listen [el type]
   (let [port (chan)
@@ -150,73 +152,77 @@
         (.strokeRect ctx (+ xp snappydxp) (+ yp snappydyp) wp hp)))))
 
 (defn sensory-data [world observer]
-  (let [{:keys [xi width yi height]} @observer]
+  (let [{:keys [bitmap]} world
+        {:keys [xi width yi height]} observer]
     (vec (for [xi (range xi (+ xi width))]
-           (subvec (nth @world xi) yi (+ yi height))))))
+           (subvec (nth bitmap xi) yi (+ yi height))))))
 
 (defn add-action-and-result [dxi dyi world observer owner]
   (go (let [port (do-xhr "/add-action-and-result"
                          {"context_id" (:server-token @observer)
                           "motor_value" [dxi dyi]
-                          "new_sensor_value" (sensory-data world observer)})
+                          "new_sensor_value" (sensory-data @world @observer)})
             response (<! port)
             log-entry {:sdr (into (sorted-set) (response "sp_output"))
                        :sensor-value (response "sensor_value")}]
         (put! (om/get-state owner :logchan) log-entry))))
 
-(defn handle-saccader-panning [{:keys [world observer] :as app} owner]
-  (go-loop []
-    (let [downevt (<! (om/get-state owner :mousedown))]
-      (when (= (.-button downevt) 0)
-        (om/set-state! owner :grabbed true)
-        (let [[kmousemove moves] (listen js/window "mousemove")
-              [kmouseup ups] (listen js/window "mouseup")]
-          (while (not= ups
-                       (let [[evt port] (alts! [moves ups])]
-                         (om/set-state! owner :dxp (- (.-clientX evt)
-                                                      (.-clientX downevt)))
-                         (om/set-state! owner :dyp (- (.-clientY evt)
-                                                      (.-clientY downevt)))
-                         port)))
-          (events/unlistenByKey kmousemove)
-          (events/unlistenByKey kmouseup)
+(defn handle-saccader-panning [app owner]
+  (let [{:keys [world observer]} app
+        {:keys [bitmap]} world
+        {obs-width :width obs-height :height} observer]
+    (go-loop []
+      (let [downevt (<! (om/get-state owner :mousedown))]
+        (when (= (.-button downevt) 0)
+          (om/set-state! owner :grabbed true)
+          (let [[kmousemove moves] (listen js/window "mousemove")
+                [kmouseup ups] (listen js/window "mouseup")]
+            (while (not= ups
+                         (let [[evt port] (alts! [moves ups])]
+                           (om/set-state! owner :dxp (- (.-clientX evt)
+                                                        (.-clientX downevt)))
+                           (om/set-state! owner :dyp (- (.-clientY evt)
+                                                        (.-clientY downevt)))
+                           port)))
+            (events/unlistenByKey kmousemove)
+            (events/unlistenByKey kmouseup)
 
-          ;; In obscure cases (e.g. javascript breakpoints)
-          ;; there are stale mousedowns sitting in the queue.
-          (while (let [[_ port] (alts! [(om/get-state owner :mousedown)]
-                                       :default :drained)]
-                   (not= :default port))))
+            ;; In obscure cases (e.g. javascript breakpoints)
+            ;; there are stale mousedowns sitting in the queue.
+            (while (let [[_ port] (alts! [(om/get-state owner :mousedown)]
+                                         :default :drained)]
+                     (not= :default port))))
 
-        (let [{:keys [dxp dyp]} (om/get-state owner)]
-          (when (not (nil? dxp))
-            (let [dxi (-> dxp
-                          abs
-                          (/ (cell-width @world owner))
-                          floor
-                          (cond-> (neg? dxp) (* -1)))
-                  dyi (-> dyp
-                          abs
-                          (/ (cell-height @world owner))
-                          floor
-                          (cond-> (neg? dyp) (* -1)))
-                  xi (+ (@observer :xi) dxi)
-                  yi (+ (@observer :yi) dyi)]
-              (when (and (<= 0 xi)
-                         (<= (+ xi (@observer :width))
-                             (world-width @world))
-                         (<= 0 yi)
-                         (<= (+ yi (@observer :height))
-                             (world-height @world)))
-                (om/transact! observer :xi #(+ % dxi))
-                (om/transact! observer :yi #(+ % dyi))
-                (add-action-and-result dxi dyi world observer owner)))
-            (om/set-state! owner :dxp nil)
-            (om/set-state! owner :dyp nil)))
-        (om/set-state! owner :grabbed false)))
-    (recur)))
+          (let [{:keys [dxp dyp]} (om/get-state owner)]
+            (when (not (nil? dxp))
+              (let [dxi (-> dxp
+                            abs
+                            (/ (cell-width @world owner))
+                            floor
+                            (cond-> (neg? dxp) (* -1)))
+                    dyi (-> dyp
+                            abs
+                            (/ (cell-height @world owner))
+                            floor
+                            (cond-> (neg? dyp) (* -1)))
+                    xi (+ (@observer :xi) dxi)
+                    yi (+ (@observer :yi) dyi)]
+                (when (and (<= 0 xi)
+                           (<= (+ xi (@observer :width))
+                               (bitmap-width @bitmap))
+                           (<= 0 yi)
+                           (<= (+ yi (@observer :height))
+                               (bitmap-height @bitmap)))
+                  (om/transact! observer :xi #(+ % dxi))
+                  (om/transact! observer :yi #(+ % dyi))
+                  (add-action-and-result dxi dyi world observer owner)))
+              (om/set-state! owner :dxp nil)
+              (om/set-state! owner :dyp nil)))
+          (om/set-state! owner :grabbed false)))
+      (recur))))
 
 (def canvas-ref "saccader-canvas")
-(defn saccader-view [{:keys [world observer] :as app} owner]
+(defn saccader-view [app owner]
   (reify
     om/IInitState
     (init-state [_]
@@ -230,63 +236,66 @@
       (handle-saccader-panning app owner)
       (go
         (let [token (<! (do-xhr "/set-initial-sensor-value"
-                                (sensory-data world observer)))]
+                                (sensory-data (:world @app)
+                                              (:observer @app))))]
           (om/update! app [:observer :server-token] token)
           (println "Server assigned us token" token))))
 
     om/IDidMount
     (did-mount [_]
-      (paint-saccader app owner)
-      )
+      (paint-saccader app owner))
 
     om/IDidUpdate
     (did-update [_ prev-props prev-state]
       (paint-saccader app owner))
 
     om/IRenderState
-    (render-state [_ {:keys [width height style grabbed mousedown]}]
-      (dom/canvas #js {:ref canvas-ref :width width :height height
-                       :className (if grabbed "grabbed" "grab")
-                       :onMouseDown (fn [e] (.persist e) (put! mousedown e))
-                       :style (clj->js style)
-                       }))))
+    (render-state [_ {:keys [style grabbed mousedown]}]
+      (let [world (:world app)]
+        (dom/canvas #js {:ref canvas-ref
+                         :width (:width-px world) :height (:height-px world)
+                         :className (if grabbed "grabbed" "grab")
+                         :onMouseDown (fn [e] (.persist e) (put! mousedown e))
+                         :style (clj->js style)
+                         })))))
 
 ;; ============================================================================
 ;; Component: world-and-observer-view
 
-(defn world-and-observer-view [{:keys [world] :as app} owner]
-  (reify
-    om/IRenderState
-    (render-state [_ {:keys [width height logchan]}]
-      (dom/div #js {:position "relative"
-                    :style #js {:width width :height height}}
-               (om/build world-view world
-                         {:init-state
-                          {:width width :height height
-                           :style {:position "absolute"
-                                   :left 0 :top 0 :zIndex 0}}})
-               (om/build saccader-view app
-                         {:init-state
-                          {:width width :height height :logchan logchan
-                           :style {:position "absolute"
-                                   :left 0 :top 0 :zIndex 1}}})))))
+(defn world-and-observer-view [app owner]
+  (let [{:keys [world]} app
+        {:keys [width-px height-px]} world]
+    (reify
+      om/IRenderState
+      (render-state [_ {:keys [logchan]}]
+        (dom/div #js {:position "relative"
+                      :style #js {:width width-px :height height-px}}
+                 (om/build world-view world
+                           {:init-state
+                            {:style {:position "absolute"
+                                     :left 0 :top 0 :zIndex 0}}})
+                 (om/build saccader-view app
+                           {:init-state
+                            {:logchan logchan
+                             :style {:position "absolute"
+                                     :left 0 :top 0 :zIndex 1}}}))))))
 
 ;; ============================================================================
 ;; Component: log-view
 
 (defn log-entry [[sensor-value sdr-log]]
   (apply dom/div nil
-           (om/build world-view sensor-value
-                     {:init-state {:width 100 :height 100}})
+         (om/build world-view {:bitmap sensor-value
+                               :width-px 100 :height-px 100})
 
-           (map (fn [{:keys [sdr count]}]
-                  (dom/div nil
-                           (dom/div #js {:style
-                                         #js {:width 100
-                                              :font-family "Consolas"}}
-                                    (string/join " " sdr))
-                           (dom/div nil count)))
-                sdr-log)))
+         (map (fn [{:keys [sdr count]}]
+                (dom/div nil
+                         (dom/div #js {:style
+                                       #js {:width 100
+                                            :font-family "Consolas"}}
+                                  (string/join " " sdr))
+                         (dom/div nil count)))
+              sdr-log)))
 
 ;; TODO - as I write this, every time I save it destroys the log.
 ;; When I find myself being annoyed by this kind of thing, I bet the right
