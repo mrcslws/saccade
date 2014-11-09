6d7622b960c7fbacc5cb58919c35f7a55ff59e5d
Marcus Lewis <mrcslws@gmail.com>
2014-11-08 19:54:50 -0800

    Decompose

--- src/cljs/saccade/htm/cloud.cljs
+++ src/cljs/saccade/htm/cloud.cljs
@@ -4,7 +4,7 @@
             [cljs.core.async :refer [<! put! chan mult tap alts!]]
             [goog.net.XhrIo :as XhrIo])
   (:require-macros [saccade.macros :refer [go-monitor]]
-                   [cljs.core.async.macros :refer [go-loop alt!]]))
+                   [cljs.core.async.macros :refer [go go-loop alt!]]))
 
 
 (defn ^:private do-xhr [command-path message]
@@ -22,51 +22,56 @@
                 "POST" message)
     port))
 
+(def ^:private impls
+  {:initialize (fn [htm-bridge _ current-sensor-value]
+                 (let [result (chan)]
+                   (go
+                     ;; Only reinitialize if we haven't initialized yet,
+                     ;; or if the lens is not where we think it should
+                     ;; be.
+                     (when (or (nil? (:server-token @htm-bridge))
+                               (not= current-sensor-value
+                                     (:sensor-value @htm-bridge)))
+                       (om/update! htm-bridge :server-token
+                                   (<!
+                                    (do-xhr "/set-initial-sensor-value"
+                                            current-sensor-value)))
+                       (om/update! htm-bridge :sensor-value
+                                   current-sensor-value))
+                     (put! result :success))
+                   result))
+   :saccade (fn [htm-bridge {:keys [sdrs]} motor-value new-sensor-value]
+              (let [result (chan)]
+                (go
+                  (let [response (<! (do-xhr
+                                      "/add-action-and-result"
+                                      {"context_id" (:server-token @htm-bridge)
+                                       "motor_value" motor-value
+                                       "new_sensor_value" new-sensor-value}))]
+                    (om/update! htm-bridge :sensor-value new-sensor-value)
+                    (put! sdrs
+                          {:sdr (into (sorted-set) (response "sp_output"))
+                           :sensor-value (response "sensor_value")})
+                    (put! result :success)))
+                result))})
+
 (defn cloud-htm-bridge [htm-bridge donemult]
   (let [commands (chan)
         sdrs (chan)]
-    (go-monitor
-     commands donemult
-     [[command checkpoint & args]]
-     (when-not
-         (= :success
-            (cond
-             (= command :initialize)
-             (let [[current-sensor-value] args]
-               ;; Only reinitialize if we haven't initialized yet,
-               ;; or if the lens is not where we think it should
-               ;; be.
-               (when (or (nil? (:server-token @htm-bridge))
-                         (not= current-sensor-value
-                               (:sensor-value @htm-bridge)))
-                 (om/update! htm-bridge :server-token
-                             (<!
-                              (do-xhr "/set-initial-sensor-value"
-                                      current-sensor-value)))
-                 (om/update! htm-bridge :sensor-value current-sensor-value))
-               :success)
+    (go-monitor commands donemult
+                [[command checkpoint & args]]
+                (when-not
+                    (= :success
+                       (if-let [impl (get impls command)]
+                         (<! (apply impl htm-bridge {:sdrs sdrs} args))
+                         (js/console.error (str "Unrecognized command: "
+                                                command))))
 
-             (= command :saccade)
-             (let [[motor-value new-sensor-value] args
-                   response (<! (do-xhr
-                                 "/add-action-and-result"
-                                 {"context_id" (:server-token @htm-bridge)
-                                  "motor_value" motor-value
-                                  "new_sensor_value" new-sensor-value}))]
-               (om/update! htm-bridge :sensor-value new-sensor-value)
-               (put! sdrs
-                     {:sdr (into (sorted-set) (response "sp_output"))
-                      :sensor-value (response "sensor_value")})
-               :success)
-
-             :else
-             (js/console.error (str "Unrecognized command: " command))))
-
-       ;; Failed.
-       (when-let [[cursor value] checkpoint]
-         (om/update! cursor value))
-       (loop []
-         (alt!
-           commands ([_] (recur))
-           :default :channels-are-drained))))
+                  ;; Failed.
+                  (when-let [[cursor value] checkpoint]
+                    (om/update! cursor value))
+                  (loop []
+                    (alt!
+                      commands ([_] (recur))
+                      :default :channels-are-drained))))
     [commands sdrs]))
